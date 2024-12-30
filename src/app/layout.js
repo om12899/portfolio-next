befb0e5062a24e9d93a464fb370120a8092449cb
Om Thakkar <43473346+om12899@users.noreply.github.com>
2024-12-30 14:58:09 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Icon from "@/assets/card.png";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
