1c6fa7c3cdb6531932030885ffc05d201397507e
Om Thakkar <43473346+om12899@users.noreply.github.com>
2024-12-30 13:16:49 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,22 @@ const meta = {
   description:
     "Creating inclusive experiences, pixel by pixel. Welcome to portfolio website of Om Thakkar!",
 };
+export const metadata = {
+  title: meta.title,
+  description: meta.description,
+  openGraph: {
+    type: "website",
+    title: meta.title,
+    description: meta.description,
+    siteName: "Om Thakkar",
+  },
+  twitter: {
+    creator: "Om Thakkar",
+    site: "@iamomthakkar",
+    title: meta.title,
+    description: meta.description,
+  },
+};
 function Home() {
   return (
     <Layout>
